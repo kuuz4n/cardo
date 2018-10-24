b94884a77076fc789184808f909e53f6ad5022f5
Kenneth G Bastian <kenneth.g.bastian@descouvre.com>
2018-10-24 13:04:26 +0800

    added comments on index files

--- index.js
+++ index.js
@@ -12,13 +12,30 @@ const aboutRouter = require('./server/routers/aboutRouter');
 const indexRouter = require('./server/routers/indexRouter');
 const port = 3300;
 
+//- Middleware: Logs the request to the server
+//- the 'dev' is formatted as ":http_verb :url :status_code :response_time ms - :res[content-length]"
+//- e.g. GET / 304 602.566 ms - -
+//- READ More: https://www.npmjs.com/package/morgan
 app.use(morgan('dev'));
-// parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }));
 
-// parse application/json
+// Middleware: Parses the value on the request body and then interprets its value
+//             so you can use it as req.body
+// e.g. req.body.title
+//      req.body.content
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+// Middleware: To serve static files such as images, CSS files, and JavaScript files
+//             found in the public folder
+// e.g. public/cardo.jpg is accessed as http://localhost:3300/cardo.jpg
+//      public/images/cardo.jpg is accessed as http://localhost:3300/images/cardo.jpg
+//
+// READ More: http://expressjs.com/en/starter/static-files.html
+app.use(express.static('public'));
+
+// Custom Middleware: Sets the req.viewModel as an object
+//                    so that we can use it on all our routers.
+// Purpose: So we do not have to redeclare the title on all our pages.
 app.use((req, res, next) => {
   req.viewModel = {
     title: 'Card - Note Taking App'
@@ -26,12 +43,12 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(express.static('public'));
-
-// NOTE: The __dirname is important for setting up the directory of the views
+// Sets our templating engine for views is Pug and its target directory
+// NOTE: The __dirname is a global Node.js variable important for setting up the directory of the views
 app.set('views', path.join(__dirname, 'server/views'));
 app.set('view engine', 'pug');
 
+// Note: The order must be maintained here
 app.use('/', indexRouter);
 app.use('/about', aboutRouter);
 app.use('/api/notes', notesRouter);
--- server/routers/indexRouter.js
+++ server/routers/indexRouter.js
@@ -5,16 +5,22 @@ const express = require('express');
 const router = express.Router(); //eslint-disable-line
 const SimpleJsonStore = require('simple-json-store');
 
-const store = new SimpleJsonStore('./data-2.json',
-  { notes: [] });
+// Initializes the data-2.json file with notes as its initial value if empty
+const store = new SimpleJsonStore('./data-2.json', { notes: [] });
 
-router.get('/', (req, res) => {
+router.get('/', function getIndexPage(req, res) {
   let viewModel = req.viewModel;
+  // We can extend the viewModel and add new properties
+  // e.g. viewModel.appName = 'Cardo';
+  //      viewModel.count = 10;
+  //      viewModel.choices = ['apple', 'orange', 'grapes'];
+  // Read more: https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/Basics
   viewModel.notes = store.get('notes');
   res.render('index.pug', viewModel);
 });
 
-router.post('/', (req, res) => {
+router.post('/', function submitNotes(req, res) {
+  // Process: Get notes from json -> Add new note -> Save the notes
   let notes = store.get('notes');
   notes.push({
     title: req.body.title,
@@ -22,6 +28,9 @@ router.post('/', (req, res) => {
     author: req.body.author
   });
   store.set('notes', notes);
+
+  //- It just reload the page on /
+  // More on redirection: https://developer.mozilla.org/en-US/docs/Web/HTTP/Redirections
   res.redirect('/');
 });
 
