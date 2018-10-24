8eaa44bea0cdf53ea6e218d30c5a426c0c8afcdd
Kenneth G Bastian <kenneth.g.bastian@descouvre.com>
2018-10-24 17:05:44 +0800

    added clear button

--- public/cardo-vue.js
+++ public/cardo-vue.js
@@ -15,6 +15,31 @@
         .catch(function(err) {
           self.notes = [];
         });
+    },
+    methods: {
+      addNote: function() {
+        var self = this;
+        var payload = {
+          title: self.title,
+          description: self.description
+        };
+        axios.post('/api/notes', payload)
+          .then(function(res) {
+            self.notes = res.data;
+            self.clear();
+            // self.notes.push({
+            //   id: 99,
+            //   title: self.title,
+            //   description: self.description
+            // });
+          })
+          .catch(function(err) {
+          });
+      },
+      clear: function() {
+        this.title = null;
+        this.description = null;
+      }
     }
   });
   console.log(cardoVue);
